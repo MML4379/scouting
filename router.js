8f1495f42f88aec263320d7503620a5f797ac8da
Michael Peace <michaelpeace937@gmail.com>
2026-03-17 16:46:11 -0400

    initial app

---
+++ router.js
@@ -0,0 +1,30 @@
+import Home from "./views/Home.js";
+
+export const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
+export const router = async () => {
+    const routes = [
+        { path: '/', view: Home() }
+    ];
+
+    const potentialMatches = routes.map(route => ({
+        route,
+        isMatch: location.pathname === route.path
+    }));
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    // Route to home if path is not found
+    if (!match) {
+        match = {
+            route: routes[0],
+            isMatch: true
+        };
+    }
+
+    // inject view into dom
+    document.getElementById('app').innerHTML = match.route.view;
+};//
